14a8400cf2e4f05ccc99c8058e77f25a46d5c9fe
Robson Ruiz <robsondorn@gmail.com>
2021-12-20 20:52:29 -0300

    fix components

--- src/pages/Oftalmologista/Oftalmologista.test.tsx
+++ src/pages/Oftalmologista/Oftalmologista.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Oftamologista from './Oftamologista';
+import Oftalmologista from './Oftalmologista';
 
 it('It should mount', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Oftamologista />, div);
+  ReactDOM.render(<Oftalmologista />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
---
+++ src/pages/Oftalmologista/Oftalmologista.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Oftalmologista.module.css';
+
+const Oftalmologista = () => (
+  <div className={styles.Oftalmologista}>
+    Oftalmologista Component
+  </div>
+);
+
+export default Oftalmologista;
