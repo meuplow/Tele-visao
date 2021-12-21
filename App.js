2aa1494cac7545413152e3564067e4e729019a9a
Robson Ruiz <robsondorn@gmail.com>
2021-12-20 23:13:45 -0300

    add text adhome page

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Bem-vindo ao Tele-visão!</Text>
+      <Pressable style={styles.button}>
+        <Text style={styles.text}>Cadastrar novo local</Text>
+      </Pressable>
+      <Pressable style={styles.button}>
+        <Text style={styles.text}>Cadastrar usuário</Text>
+      </Pressable>
+      <Pressable style={styles.button}>
+        <Text style={styles.text}>Login</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +24,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  title: {
+    marginBottom: 16,
+    textAlign: 'center',
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  button: {
+    marginTop: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    backgroundColor: '#6A79A8',
+    width: 200
+  },
+  text: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold'
+  }
 });
