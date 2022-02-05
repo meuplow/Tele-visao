1d7a311ace72ea66268ce2e405653ac9f03b4441
Robson Ruiz <robsondorn@gmail.com>
2022-02-05 19:04:37 -0300

    corrige campo de imagem

--- views/Examinador/Exame.js
+++ views/Examinador/Exame.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Text, TextInput, View, Pressable } from 'react-native';
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import * as ImagePicker from 'expo-image-picker';
 
 import styles from '../styles.js';
 
 export default function Exame({navigation}) {
-    function abre_galeria(){
-        launchImageLibrary(result)
-
-        const result = async () => await launchImageLibrary();
-    }
+    const pickImage = async () => {
+      // No permissions request is necessary for launching the image library
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+  
+      console.log(result);
+    };
 
     return (
         <View style={styles.container}>
-            <Pressable style={styles.list_button} onPress={abre_galeria()}>
+            <Pressable style={styles.list_button} onPress={pickImage}>
                 <Text style={styles.field_name}>Selecionar imagem da galeria</Text>
             </Pressable>
             <Text style={styles.field_name}>Descrição</Text>
