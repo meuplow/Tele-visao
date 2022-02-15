ff8e576b16e7da23e7974136996390996273a11a
Julia Martins <julia.geremias.martins@gmail.com>
2022-02-15 20:15:47 -0300

    Conexão com o Firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import * as firebase from "firebase";
+// import * as firebase from "firebase";
 import "firebase/storage";
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+    // Follow this pattern to import other Firebase services
+    // import {} from "https://www.gstatic.com/firebasejs/${FIREBASE_VERSION}/firebase-analytics.js";
+    // import {} from "https://www.gstatic.com/firebasejs/${FIREBASE_VERSION}/firebase-app-check.js";
+    // import {} from "https://www.gstatic.com/firebasejs/${FIREBASE_VERSION}/firebase-auth.js";
+    // import {} from "https://www.gstatic.com/firebasejs/${FIREBASE_VERSION}/firebase-functions.js";
+    // import {} from "https://www.gstatic.com/firebasejs/${FIREBASE_VERSION}/firebase-firestore.js";
+    // import {} from "https://www.gstatic.com/firebasejs/${FIREBASE_VERSION}/firebase-storage.js";
+    // import {} from "https://www.gstatic.com/firebasejs/${FIREBASE_VERSION}/firebase-performance.js";
+    // import {} from "https://www.gstatic.com/firebasejs/${FIREBASE_VERSION}/firebase-remote-config.js";
+    // import {} from "https://www.gstatic.com/firebasejs/${FIREBASE_VERSION}/firebase-messaging.js";
+    // import {} from "https://www.gstatic.com/firebasejs/${FIREBASE_VERSION}/firebase-database.js";
+    
+    // TODO: Replace the following with your app's Firebase project configuration
 
 const firebaseConfig = {
   apiKey: "AIzaSyBEYRR_3d9n2WQSevS5N4qd4Y1iVQI2ivQ",
@@ -18,4 +32,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export const database = firebase.Firestore();
+export const db = getFirestore(app);
+/*
+const db = firebaseApp.firestone();
+export {db};*/
--- views/Examinador/Exame.js
+++ views/Examinador/Exame.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Text, TextInput, View, Pressable } from 'react-native';
+import { Text, TextInput, View, Pressable, Button } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 import styles from '../styles.js';
+import { db } from '../../src/config/firebase.js';
+import { getFirestore, collection, getDocs, addDoc} from 'firebase/firestore';
+
+function descExame(exame) {
+    // let exame = "new exame"
+    addDoc(collection(db, "exames"), {
+        descricao: exame
+      });
+};
 
 export default function Exame({navigation}) {
     const pickImage = async () => {
@@ -16,6 +25,7 @@ export default function Exame({navigation}) {
   
       console.log(result);
     };
+    const [text, setText] = useState('');
 
     return (
         <View style={styles.container}>
@@ -23,8 +33,14 @@ export default function Exame({navigation}) {
                 <Text style={styles.field_name}>Selecionar imagem da galeria</Text>
             </Pressable>
             <Text style={styles.field_name}>Descrição</Text>
-            <TextInput multiline={true} style={styles.big_field} placeholder="Informe o olho que a imagem foi capturada e demais informações relevantes" />
-            <Pressable style={styles.button}>
+            <TextInput 
+                onChangeText={newText => setText(newText)}
+                defaultValue={text}
+                id="descricao"
+                multiline={true}
+                style={styles.big_field}
+                placeholder="Informe o olho que a imagem foi capturada e demais informações relevantes" />
+            <Pressable onPress={()=>descExame(text)} style={styles.button}>
                 <Text style={styles.text}>Enviar exame</Text>
             </Pressable>
             <StatusBar style="auto" />
