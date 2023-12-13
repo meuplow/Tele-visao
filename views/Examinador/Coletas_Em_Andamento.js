2cd5480063e8c94a253ff0aa4b5f42ab166b9354
Juliana Severo <julianamellosevero@gmail.com>
2023-12-13 18:54:27 -0300

    chore: update info messages and icon

--- views/Examinador/Coletas_Em_Andamento.js
+++ views/Examinador/Coletas_Em_Andamento.js
@@ -30,8 +30,9 @@ export default function Coletas_Em_Andamento({navigation}) {
         }
     }
 
-    function start_exam(patient){
+  function start_exam(patient){
         var response = confirm("Começar coleta?");
+        var response = confirm("Deseja iniciar a coleta?");
 
         if(response){
             navigation.navigate('Exame', {
@@ -63,8 +64,8 @@ export default function Coletas_Em_Andamento({navigation}) {
                     return (
                         <Pressable key={patient.id} style={styles.list_button} onPress={() => start_exam(patient)}>  
                             <View style={styles.list_button_local}>
-                                <Icon style={styles.camera_icon} name="hospital" color='#363636' size={20}/>
-                                <Text style={styles.subtitle}>{patient['dados']["local"]}</Text>
+                                <Icon name="hospital" size={25}/>
+                                <Text style={styles.list_subtitle}> {patient['dados']["local"]}</Text>
                             </View>
                             <Text style={styles.patientText}>Paciente: {patient['dados']["nome_completo"]}</Text>
                         </Pressable>
