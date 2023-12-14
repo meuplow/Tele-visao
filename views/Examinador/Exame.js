68ac5a0323f759b1a83a4a45e0d99047c1e23f7c
Juliana Severo <julianamellosevero@gmail.com>
2023-12-14 18:54:12 -0300

    feat: creating images array

--- views/Examinador/Exame.js
+++ views/Examinador/Exame.js
@@ -15,28 +15,30 @@ import loginStyle from '../Login/LoginStyle.js';
 
 
 class ExamInfo {
-    constructor(image, description) {
-        this.image = image;
+    constructor(images, description) {
+        this.images = images;
         this.description = description;
     }
 }
 
-async function uploadImage(image, id) {
-    const { uri } = image;
-    const filename = 'imagens_exames/' + id + '.jpg';
+async function uploadImage(images, id) {
+    const imageUrls = [];
 
-    // caso seja exportado para iOS, usar essa linha
-    // const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri;
-
-    const imageBase64 = uri.replace('data:image/jpeg;base64,', '');
-
-    const imageBytes = Buffer.from(imageBase64, "base64");
-
-    const imageRef = ref(storage, filename);
-    const result = await uploadBytes(imageRef, imageBytes);
-
-    return filename;
-}
+    for (const image of images) {
+      const { uri } = image;
+      const filename = 'imagens_exames/' + id + '_' + imageUrls.length + '.jpg';
+  
+      const imageBase64 = uri.replace('data:image/jpeg;base64,', '');
+      const imageBytes = Buffer.from(imageBase64, "base64");
+  
+      const imageRef = ref(storage, filename);
+      await uploadBytes(imageRef, imageBytes);
+  
+      imageUrls.push(filename);
+    }
+  
+    return imageUrls;
+  }
 
 async function addExamInfo(patient, examInfo) {
     let patientData = patient['dados'];
@@ -45,13 +47,13 @@ async function addExamInfo(patient, examInfo) {
     const patientRef = doc(db, 'exames', patient['id']);
 
     // envia imagem para o Firebase Storage
-    const imageUrl = await uploadImage(examInfo.image, patient['id']);
-    patientData['image'] = imageUrl;
+    const imageUrl = await uploadImage(examInfo.images, patient['id']);
+    patientData['images'] = imageUrl;
     patientData['description'] = examInfo.description;
 
     // atualiza somente os campos image e description a partir de um JSON
     await updateDoc(patientRef, {
-        'image': imageUrl,
+        'images': imageUrl,
         'description': examInfo.description,
         'coletado': true,
     });
@@ -62,6 +64,9 @@ export default function Exame({ route, navigation }) {
 
     const { patient } = route.params;
 
+    const [description, setDescription] = useState('');
+    const [images, setImages] = useState([]);
+  
     const getUserNavigate = async () => {
         let users = new Array();
         let userRef = collection(db, "usuario");
@@ -83,14 +88,16 @@ export default function Exame({ route, navigation }) {
         aspect: [4, 3],
         quality: 1,
       });
-        setImage(result);
+
+        if (!result.cancelled) {
+            // Add a imagem da coleta ao array de imagens
+            setImages([...images, result]);
+        }
         return result;
     };
-    const [description, setDescription] = useState('');
-    const [image, setImage] = useState(null);
-
+ 
     const addInfo = async (patient, examInfo) => {
-        console.log(image)
+        console.log(images)
         await addExamInfo(patient, examInfo);
     };
 
@@ -99,10 +106,17 @@ export default function Exame({ route, navigation }) {
     }
 
     const uploadExam = async (patient, examInfo) => {
-        addInfo(patient, new ExamInfo(image, description));
+        addInfo(patient, new ExamInfo(images, description));
         simpleAlert();
         getUserNavigate();
     }
+
+    const renderImageInfo = (images, index) => (
+        <View key={index} style={styles.container_exame}>
+            <Icon_person style={styles.camera_icon} name="check" size={20} color="#A3E8A3" />
+            <Text style={styles.subtitle}>Imagem {index + 1} Carregada</Text>
+        </View>
+    );
 
     return (
         <View style={styles.containerCentralize}>
@@ -115,11 +129,15 @@ export default function Exame({ route, navigation }) {
             <Text style={styles.field_name}> Solicitação: {patient['dados']['infos_solicitacao']}</Text>
             </View>
             <View style={styles.container_exame}>
-                <Pressable style={styles.list_button} onPress={ () => pickImage() }>
-                    <Text style={styles.field} > Clique aqui e selecione a imagem do exame na galeria </Text>
-                    <Icon name="upload" size={30} color='#4864b0' />
-                </Pressable></View>
-            {image&& <View style={styles.container_exame}>
+            <Pressable style={styles.list_button} onPress={() => pickImage()}>
+                 <Text style={styles.field}> Clique aqui e selecione a imagem do exame na galeria </Text>
+                 <Icon name="upload" size={30} color="#4864b0" />
+             </Pressable>
+
+             {images.map(renderImageInfo)}
+
+            </View>
+            {images&& <View style={styles.container_exame}>
                 <Icon_person style={styles.camera_icon} name="check" size={20} color='#A3E8A3'/>
                 <Text style={styles.subtitle}>Imagem Carregada</Text>
             </View>}
@@ -132,10 +150,11 @@ export default function Exame({ route, navigation }) {
                     style={styles.big_field}
                     placeholder="Informe o olho que a imagem foi capturada e demais informações relevantes" />
             <View style={styles.container_exame}>
-            <Pressable onPress={ () => uploadExam(patient, new ExamInfo(image, description)) } style={styles.button_exam}>
-                <Text style={styles.text}>Enviar exame</Text>
-            </Pressable></View>
+            <Pressable onPress={() => uploadExam(patient, new ExamInfo(images, description))} style={styles.button_exam}>
+                 <Text style={styles.text}>Enviar exame</Text>
+             </Pressable>
             <StatusBar style="auto" />
+           </View>
         </View>
     );
 }
